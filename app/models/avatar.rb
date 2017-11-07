26b7c702d231b83a1a1efed292f623c20eac03b4
konada <konrad.cisak@gmail.com>
2017-11-07 22:28:26 +0100

    Add validations to avatar model

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -1,3 +1,14 @@
 class Avatar < ActiveRecord::Base
   belongs_to :source
+
+  has_attached_file :image, default_url: 'images/avatar.png'
+  validates_attachment :image,
+                       content_type:
+                        {
+                          content_type: [
+                            'image/jpeg',
+                            'image/jpg',
+                            'image/png'
+                          ]
+                        }
 end
